5063872760f6d02bf3e18e8b3d038866dad37f91
Chris (from laptop/git desktop app) <38088640+chuynh18@users.noreply.github.com>
2018-08-14 21:08:54 -0700

    working first version

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,6 +25,7 @@ const game = {
     snake: [[8,12]],
     fruit: [null, null],
     ateFruit: false,
+    collided: false,
     setDirection: function(input) {
         if (input === 0) {
             this.direction = 0;
@@ -38,8 +39,9 @@ const game = {
     },
     checkSelfCollision: function() {
         let collision = false;
+
         if (this.snake.length > 1) {
-            for (let i = 1; i < snake.length; i ++) {
+            for (let i = 1; i < this.snake.length; i ++) {
                 if (this.snake[0][0] === this.snake[i][0] && this.snake[0][1] === this.snake[i][1]) {
                     collision = true;
                     break;
@@ -52,8 +54,8 @@ const game = {
         }
     },
     generateFruit: function() {
-        this.fruit[0] = Math.floor(Math.random() * 15);
-        this.fruit[1] = Math.floor(Math.random() * 17);
+        this.fruit[0] = Math.floor(Math.random() * 17);
+        this.fruit[1] = Math.floor(Math.random() * 15);
     },
     reset: function() {
         this.speed = 500;
@@ -77,6 +79,7 @@ const game = {
             }
         }
 
+        console.log(`${this.fruit[0]}-${this.fruit[1]}`);
         document.getElementById(`${this.fruit[0]}-${this.fruit[1]}`).classList.add("fruit");
         document.getElementById(`${this.snake[0][0]}-${this.snake[0][1]}`).classList.add("snake-head");
 
@@ -90,17 +93,37 @@ const game = {
         let lastLocation;
 
         if (this.direction === 0) {
-            this.snake.unshift([this.snake[0][0], this.snake[0][1] - 1]);
+            if (this.snake[0][1] === 0) {
+                console.log("You collided with the wall!");
+                this.collided = true;
+            } else {
+                this.snake.unshift([this.snake[0][0], this.snake[0][1] - 1]);
             lastLocation = this.snake.pop();
+            }
         } else if (this.direction === 1) {
-            this.snake.unshift([this.snake[0][0] + 1, this.snake[0][1]]);
-            lastLocation = this.snake.pop();
+            if (this.snake[0][0] === 16) {
+                console.log("You collided with the wall!");
+                this.collided = true;
+            } else {
+                this.snake.unshift([this.snake[0][0] + 1, this.snake[0][1]]);
+                lastLocation = this.snake.pop();
+            }
         } else if (this.direction === 2) {
-            this.snake.unshift([this.snake[0][0], this.snake[0][1] + 1]);
-            lastLocation = this.snake.pop();
+            if (this.snake[0][1] === 14) {
+                console.log("You collided with the wall!");
+                this.collided = true;
+            } else {
+                this.snake.unshift([this.snake[0][0], this.snake[0][1] + 1]);
+                lastLocation = this.snake.pop();
+            }
         } else if (this.direction === 3) {
-            this.snake.unshift([this.snake[0][0] - 1, this.snake[0][1]]);
-            lastLocation = this.snake.pop();
+            if (this.snake[0][0] === 0) {
+                console.log("You collided with the wall!");
+                this.collided = true;
+            } else {
+                this.snake.unshift([this.snake[0][0] - 1, this.snake[0][1]]);
+                lastLocation = this.snake.pop();
+            }
         }
 
         if (this.ateFruit) {
@@ -108,16 +131,56 @@ const game = {
             this.snake.push(lastLocation);
         }
 
+        if (this.checkSelfCollision()) {
+            console.log("You collided with yourself!");
+            this.collided = true;
+        }
         this.eatFruit();
         this.render();
+
+        if (!this.collided) {
+            setTimeout(function() {
+                game.move();
+            }, this.speed);
+        } else {
+            console.log("Game over.");
+        }
     },
     eatFruit: function() {
         if (this.snake[0][0] === this.fruit[0] && this.snake[0][1] === this.fruit[1]) {
             this.ateFruit = true;
             this.generateFruit();
+            this.newHighScore();
+        }
+    },
+    newHighScore: function() {
+        if (this.snake.length > this.highScore) {
+            this.highScore = this.snake.length;
+            console.log(`New high score: ${this.highScore}`);
         }
     }
 };
+
+const checkKey = function(e) {
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        // up arrow
+        game.setDirection(0);
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+        game.setDirection(2);
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+       game.setDirection(3);
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+       game.setDirection(1);
+    }
+}
 
 const start = function() {
     game.generateFruit();
@@ -125,3 +188,4 @@ const start = function() {
 }
 
 start();
+document.onkeydown = checkKey;
